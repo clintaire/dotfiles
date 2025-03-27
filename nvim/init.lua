e581bbfcd49b3fadea75d934da8c00b5b51e260c
Clint Aire <111376518+clintaire@users.noreply.github.com>
2025-03-27 12:00:02 +0000

    feat: add comprehensive Neovim Lua configuration for plugins, LSP, and LaTeX setup

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,44 +1,32 @@
--- stuff that must be at the very beginning
+--
+-- ███╗   ██╗███████╗ ██████╗ ██╗   ██╗██╗███╗   ███╗
+-- ████╗  ██║██╔════╝██╔═══██╗██║   ██║██║████╗ ████║
+-- ██╔██╗ ██║█████╗  ██║   ██║██║   ██║██║██╔████╔██║
+-- ██║╚██╗██║██╔══╝  ██║   ██║╚██╗ ██╔╝██║██║╚██╔╝██║
+-- ██║ ╚████║███████╗╚██████╔╝ ╚████╔╝ ██║██║ ╚═╝ ██║
+-- ╚═╝  ╚═══╝╚══════╝ ╚═════╝   ╚═══╝  ╚═╝╚═╝     ╚═╝
+--
+-- Thanks @ Arfan Zubi
+-- https://github.com/3rfaan/dotfiles
+-- Neovim Lua Config File by Arfan Zubi
+-- INIT FILE
 
--- inspired by Primeagen, let's see how it goes
-vim.g.mapleader = " "
+-- Variables
+opt = vim.opt
+g = vim.g
+kmap = vim.keymap
+cmd = vim.cmd
 
--- helpful function for debugging:
--- from: https://github.com/nanotee/nvim-lua-guide#tips-2
-function _G.dump(...)
-    local objects = vim.tbl_map(vim.inspect, { ... })
-    print(unpack(objects))
-end
+-- Leader key
+g.mapleader = ","
 
--- to evaluate vim in lua
--- vim.api.nvim_eval('1 + 1')
+-- Imports
+require("core.lazy")       -- lazy.nvim plugin manager
+require("core.utils")      -- Utility functions
+require("core.mappings")   -- Mappings
+require("core.scripts")    -- Scripts
+require("core.settings")   -- Editor settings
 
--- to execute command in lua
--- vim.api.nvim_command('set nonumber') or vim.cmd()
-
--- getting/setting vim options - https://github.com/nanotee/nvim-lua-guide#using-api-functions
--- alternative: https://github.com/nanotee/nvim-lua-guide#using-meta-accessors
-
--- getting/setting vim global vars - vim.api.nvim_set_var()
--- https://github.com/nanotee/nvim-lua-guide#using-api-functions-1
--- alternative: https://github.com/nanotee/nvim-lua-guide#using-meta-accessors-1
-
--- keymaps - https://github.com/nanotee/nvim-lua-guide#defining-mappings
-
--- helper function to automatically escape term keycodes
--- ref: https://github.com/nanotee/nvim-lua-guide#vimapinvim_replace_termcodes
--- The function is called `t` for `termcodes`.
--- You don't have to call it that, but I find the terseness convenient
-function t(str)
-    -- Adjust boolean arguments as needed
-    return vim.api.nvim_replace_termcodes(str, true, true, true)
-end
-
-DATA_PATH = vim.fn.stdpath("data")
-
-require("lazy_conf")
-require("misc")
-require("keybinds")
-require("neovide_conf")
-require("statusline_conf")
-vim.cmd("source " .. vim.fn.stdpath("config") .. "/vimscript/legacy.vim")
+require("plugins.configs") -- All setups and configurations
+require("plugins.plugins") -- Plugins
+require("plugins.setups")  -- Setup of plugins
